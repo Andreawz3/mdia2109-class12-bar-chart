b7d3f66b32546bece4f14d67499e99373b23e969
Andrea Wu <andrea.wzg@gmail.com>
2023-03-20 10:54:38 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Chart.js 2
+https://react-chartjs-2.js.org/examples
+
+## Dependancy Installation
+```
+npm i react-chartjs-2
+```
+
+## Doughnut Chart
+
+## Collected Data
+[Bank of Canada](https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=1010013601)
+
---
+++ components/BarChart/index.js
@@ -0,0 +1,68 @@
+import { useState, useEffect } from "react"
+import styles from './BarChart.module.css'
+
+import { Bar } from 'react-chartjs-2'
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+} from 'chart.js'
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+)
+
+export default function BarChart() {
+
+    const [chartData, setChartData] = useState({
+        datasets: []
+    })
+
+    const [chartOptions, setChartOptions] = useState ({})
+
+    useEffect(() => {
+        setChartData({
+            labels: ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"],
+            datasets: [
+                {
+                    labels: "Sales $",
+                    data: [15235, 12553, 21635, 13214, 19245, 16524, 14295],
+                    borderColor: 'rgb(53, 162, 235)',
+                    backgroundColor: 'rgba(53, 162, 235, 0.4)'
+                }
+            ]
+        })
+
+        setChartOptions({
+            plugins: {
+                legend: {
+                    position: 'top'
+                },
+                title: {
+                    display: true,
+                    text: "Daily Revenue"
+                }
+            },
+            maintainAspectRation: false,
+            responsive: true
+        })
+
+    }, [])
+
+    return (
+        <>
+            <div className={styles.container}>
+                <Bar data={chartData} option={chartOptions} />
+            </div>
+        </>
+    )
+}
